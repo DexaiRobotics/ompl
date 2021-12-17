3e7c3cbdf1c86df96c8ef85841b8844f0ddde6f0
Ramy Yammine <ramy.yammine@dexai.com>
2021-12-17 18:20:48 +0000

    more meaningful comment for cost passing to solutions from problem_def

--- src/ompl/base/src/ProblemDefinition.cpp
+++ src/ompl/base/src/ProblemDefinition.cpp
@@ -426,7 +426,8 @@ void ompl::base::ProblemDefinition::addSolutionPath(const PathPtr &path, bool ap
 {
     PlannerSolution sol(path);
 
-    // 2 lines added by Ramy
+    // These 2 lines insure that the cost function we specify is passed to the solutions.
+    // The cost function will now be used in the < operator between solutions, and hence sorting will be according to cost.
     Cost sol_cost {path->cost(optimizationObjective_)};
     sol.setOptimized(optimizationObjective_, sol_cost, false);
     
