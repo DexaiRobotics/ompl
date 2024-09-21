3702b390d9039fb6750ab6ff454ac8bcb0f67910
yammineramy <88019967+yammineramy@users.noreply.github.com>
2024-09-20 23:58:32 -0400

    Update SPARSdb.cpp

--- src/ompl/tools/thunder/src/SPARSdb.cpp
+++ src/ompl/tools/thunder/src/SPARSdb.cpp
@@ -1312,8 +1312,11 @@ void ompl::geometric::SPARSdb::findGraphNeighbors(base::State *st, std::vector<V
     visibleNeighborhood.clear();
     stateProperty_[queryVertex_] = st;
     if (!radius.has_value()) {
+        OMPL_INFORM("No radius provided, using sparseDelta_");
         radius = sparseDelta_;
     }
+    OMPL_INFORM("radius: %f", *radius);
+    OMPL_INFORM("sparseDelta_: %f", sparseDelta_);
     nn_->nearestR(queryVertex_, *radius, graphNeighborhood);
     if (verbose_ && false)
         OMPL_INFORM("Finding nearest nodes in NN tree within radius %f", *radius);
